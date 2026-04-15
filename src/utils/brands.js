811acca76f4e6d0c2ae22f06419c8e8be87291a9
majoroka <majoroka@gmail.com>
2026-04-15 18:48:28 +0100

    Expande catálogo de marcas com metadados e marcas próprias

--- src/utils/brands.js
+++ src/utils/brands.js
@@ -1,13 +1,16 @@
 import baseBrands from "../data/brands.json";
 import { uniqueValues } from "./helpers.js";
 
+function getBrandName(brand) {
+  return typeof brand === "string" ? brand : brand.name;
+}
+
 export function getBrandOptions({ basket = [], results = [] } = {}) {
   const importedBasketBrands = basket.map((item) => item.preferredBrand);
   const importedResultBrands = results.map((result) => result.brand);
 
-  return uniqueValues([...baseBrands, ...importedBasketBrands, ...importedResultBrands])
+  return uniqueValues([...baseBrands.map(getBrandName), ...importedBasketBrands, ...importedResultBrands])
     .map((brand) => String(brand).trim())
     .filter(Boolean)
     .sort((left, right) => left.localeCompare(right, "pt-PT", { sensitivity: "base" }));
 }
-
